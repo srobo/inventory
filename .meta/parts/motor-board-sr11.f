2b394eeb9c996460430d3cc5df724082534dce38
Richard Barlow <richard@richardbarlow.co.uk>
2011-06-01 21:44:36 +0100

    Add an SR11.F motor board template

---
+++ .meta/parts/motor-board-sr11.f
@@ -0,0 +1,16 @@
+assetcode : [ASSET_CODE]
+labelled  : false   # Does the asset have its assetcode marked on it
+revision  : 0
+
+description: >
+    A SR11.F motor board.
+
+purchasing_ticket : 0         # The trac ticket number of the purchase request
+value             : 28.00     # A rough estimate of the value of the asset
+condition         : unknown   # One of {unknown, working, broken}
+
+tested            : false     # Has the asset been tested
+cased             : false     # Has the asset been put into a case
+
+photos:
+   - http://www.flickr.com/photos/rspanton/5406066865/
