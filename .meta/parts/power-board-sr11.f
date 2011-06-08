db9ae830bd4aa698846cb9cebe83984da697a447
Jeremy Morse <jmorse+git@studentrobotics.org>
2011-06-08 19:39:52 +0100

    Correct unrecorded value of power board

--- .meta/parts/power-board-sr11.f
+++ .meta/parts/power-board-sr11.f
@@ -6,7 +6,7 @@ description: >
     SR2011 Power board
 
 purchasing_ticket : 0         # The trac ticket number of the purchase request
-value             : 0.00      # A rough estimate of the value of the asset
+value             : 70.00     # A rough estimate of the value of the asset
 condition         : unknown   # One of {unknown, working, broken}
 
 tvs-mod-698       : true      # Has TVS diode mod 698 been applied?
