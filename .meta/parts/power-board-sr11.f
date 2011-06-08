f4fd8ccd5400f0cc99ec569cf2bb6b1e81c599b5
Jeremy Morse <jmorse+git@studentrobotics.org>
2011-06-08 19:13:56 +0100

    Add power board asset

---
+++ .meta/parts/power-board-sr11.f
@@ -0,0 +1,12 @@
+assetcode : [ASSET_CODE]
+labelled  : false   # Does the asset have its assetcode marked on it
+revision  : 0
+
+description: >
+    SR2011 Power board
+
+purchasing_ticket : 0         # The trac ticket number of the purchase request
+value             : 0.00      # A rough estimate of the value of the asset
+condition         : unknown   # One of {unknown, working, broken}
+
+tvs-mod-698       : true      # Has TVS diode mod 698 been applied?
