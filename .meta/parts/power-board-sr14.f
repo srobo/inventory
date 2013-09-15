9a0a04cdc208f44c028b6e2a10b2f21e4bc059cf
Rob Spanton <rspanton@zepler.net>
2013-09-15 19:50:07 +0100

    Fix SR year in new power board template
    I failed to update this to SR2014.

--- .meta/parts/power-board-sr14.f
+++ .meta/parts/power-board-sr14.f
@@ -3,7 +3,7 @@ labelled  : false   # Does the asset have its assetcode marked on it
 revision  : 0
 
 description: >
-    SR2013 Power board
+    SR2014 Power board
 
 purchasing_ticket : 1778      # The trac ticket number of the purchase request
 value             : 110.66    # A rough estimate of the value of the asset
